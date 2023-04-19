305e343811b2a987cdd7180eca7bd5f8da0b3ee0
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-04-19 12:52:01 -0300

    adicionando itens no local storage e refatorando

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
 let novaTarefa = document.getElementById("novaTarefa")
 let listaTarefas = document.getElementById("listaTarefas")
 let inputTarefa = document.getElementById("inputTarefa")
+let tarefasArr = JSON.parse(localStorage.getItem("tarefas")) || []
+
+tarefasArr.forEach((el) => {
+  criaItem(el)
+})
 
 novaTarefa.addEventListener("submit", function(e) {
   e.preventDefault()
-  //console.log("TAREFA CRIADA")
 
-  // cria div pai
-  let novoItem = criarElemento("div") 
-  novoItem.classList.add("tarefa_item")  
+  criaItem(inputTarefa.value,)
 
-  // cria div do nome da tarefa
-  let nomeTarefa = criarElemento("div")
-  nomeTarefa.classList.add("nome_tarefa")
-  
-  // add texto na div da tarefa nova
-  nomeTarefa.innerHTML += `<p>${inputTarefa.value}</p>`
-
-  // add div do nome da tarefa ao novo item
-  novoItem.appendChild(nomeTarefa)
-
-  // add novo item à lista de tarefas
-  listaTarefas.appendChild(novoItem)
-
-  // add checkbox a div do nome da tarefa (funcionou)
-  nomeTarefa.appendChild(criaCheckbox())
-
-  // add botao de delete
-  novoItem.appendChild(criaBtnDelete())
+  // add ao local storage
+  tarefasArr.push(inputTarefa.value)
+  localStorage.setItem("tarefas", JSON.stringify(tarefasArr)) 
 
   // limpa input
   inputTarefa.value = ""
-
 })
 
 function criaCheckbox() {
-
   const checkboxEl = criarElemento("input")
   checkboxEl.setAttribute("type", "checkbox")
 
@@ -68,9 +53,42 @@ function criaBtnDelete() {
   buttonEl.addEventListener("click", function(e) {
     const parentEl = e.target.parentElement.parentElement
     parentEl.remove()
+
+    // remover item do local storage
+    const textElement = divBtn.previousElementSibling.firstChild
+    const indexElRemove = tarefasArr.indexOf(textElement.textContent)
+    tarefasArr.splice(indexElRemove, 1)
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefasArr))
   })
 
   return divBtn
+}
+
+function criaItem(tarefa) {
+
+  // cria div pai
+  let novoItem = criarElemento("div") 
+  novoItem.classList.add("tarefa_item")  
+
+  // cria div do nome da tarefa
+  let nomeTarefa = criarElemento("div")
+  nomeTarefa.classList.add("nome_tarefa")
+  
+  // add texto na div da tarefa nova
+  nomeTarefa.innerHTML += `<p>${tarefa}</p>`
+
+  // add div do nome da tarefa ao novo item
+  novoItem.appendChild(nomeTarefa)
+
+  // add novo item à lista de tarefas
+  listaTarefas.appendChild(novoItem)
+
+  // add checkbox a div do nome da tarefa (funcionou)
+  nomeTarefa.appendChild(criaCheckbox())
+
+  // add botao de delete
+  novoItem.appendChild(criaBtnDelete())
 }
 
 function criarElemento(tagName) {
