7067084b9d6df0729711a4923bf37ac2c46481fc
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-04-20 19:37:58 -0300

    identificar tarefas marcadas mesmo após atualizar a página

--- app.js
+++ app.js
@@ -3,36 +3,61 @@ let listaTarefas = document.getElementById("listaTarefas")
 let inputTarefa = document.getElementById("inputTarefa")
 let tarefasArr = JSON.parse(localStorage.getItem("tarefas")) || []
 
-tarefasArr.forEach((el) => {
-  criaItem(el)
+tarefasArr.forEach((tarefa) => {
+  criaItem(tarefa)
 })
 
 novaTarefa.addEventListener("submit", function(e) {
   e.preventDefault()
 
-  criaItem(inputTarefa.value,)
+  const tarefaAtual = {
+    nome: inputTarefa.value,
+    marcada: false
+  }
+
+  tarefaAtual.id = tarefasArr[tarefasArr.length - 1] ? (tarefasArr[tarefasArr.length - 1]).id + 1 : 0
+
+  criaItem(tarefaAtual)
 
   // add ao local storage
-  tarefasArr.push(inputTarefa.value)
+  tarefasArr.push(tarefaAtual)
   localStorage.setItem("tarefas", JSON.stringify(tarefasArr)) 
 
   // limpa input
   inputTarefa.value = ""
 })
 
-function criaCheckbox() {
+function criaCheckbox(tarefa) {
   const checkboxEl = criarElemento("input")
   checkboxEl.setAttribute("type", "checkbox")
 
   checkboxEl.addEventListener("click", function(e) {
+
     const isChecked = e.target.checked
     const textoTarefa = e.target.previousElementSibling
 
     if (isChecked) {
       textoTarefa.style.textDecoration = "line-through"
 
+      // altera propriedade `marcada` do objeto identificando se foi marcada 
+      tarefa.marcada = true
+
     } else {
       textoTarefa.style.textDecoration = "none"
+      tarefa.marcada = false
+    }
+
+    // reescrevo o local storage para salvar marcada
+    localStorage.setItem("tarefas", JSON.stringify(tarefasArr)) 
+  })
+
+  // checkbox continuar marcado e texto riscado após atualizar a página de acordo com o local storage  
+  window.addEventListener('load', function() {
+    if (tarefa.marcada == true) {
+
+      checkboxEl.checked = true
+      const pElement = checkboxEl.previousSibling
+      pElement.style.textDecoration = "line-through" 
     }
   })
 
@@ -57,6 +82,8 @@ function criaBtnDelete() {
     // remover item do local storage
     const textElement = divBtn.previousElementSibling.firstChild
     const indexElRemove = tarefasArr.indexOf(textElement.textContent)
+
+    console.log(indexElRemove)
     tarefasArr.splice(indexElRemove, 1)
 
     localStorage.setItem("tarefas", JSON.stringify(tarefasArr))
@@ -76,7 +103,10 @@ function criaItem(tarefa) {
   nomeTarefa.classList.add("nome_tarefa")
   
   // add texto na div da tarefa nova
-  nomeTarefa.innerHTML += `<p>${tarefa}</p>`
+  let textoTarefa = criarElemento("p")
+  textoTarefa.innerText = tarefa.nome
+
+  nomeTarefa.appendChild(textoTarefa)
 
   // add div do nome da tarefa ao novo item
   novoItem.appendChild(nomeTarefa)
@@ -85,7 +115,7 @@ function criaItem(tarefa) {
   listaTarefas.appendChild(novoItem)
 
   // add checkbox a div do nome da tarefa (funcionou)
-  nomeTarefa.appendChild(criaCheckbox())
+  nomeTarefa.appendChild(criaCheckbox(tarefa))
 
   // add botao de delete
   novoItem.appendChild(criaBtnDelete())
